18229a2b9ff6dbeb111892866a2c6c9ae7b07ba0
Thomas Roth <thomas.roth@nist.gov>
2018-02-09 21:38:41 +0000

    fixed error that made test federate RO

--- TestFederate/TestFederation-java-federates/TestFederation-base-java/src/main/java/TestFederation/TestFederateBase.java
+++ TestFederate/TestFederation-java-federates/TestFederation-base-java/src/main/java/TestFederation/TestFederateBase.java
@@ -26,6 +26,7 @@ public class TestFederateBase extends SynchronizedFederate {
 
 		enableTimeConstrained();
 
+		enableTimeRegulation(getLookAhead());
 		enableAsynchronousDelivery();
         // interaction pubsub
         
